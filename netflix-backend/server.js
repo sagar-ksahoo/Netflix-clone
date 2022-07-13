9e965b4f4a7cf5df32c5b93f0950cc449d49b96c
sagar-ksahoo <sagarsahoo1729@gmail.com>
2022-07-13 13:48:51 +0530

    jwt auth created

---
+++ netflix-backend/server.js
@@ -0,0 +1,26 @@
+//db pswd : VYQeGniQjVYs8AUQ
+
+const express = require("express")
+const app = express()
+
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then( () => console.log("DB connected successfully!"))
+.catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/server/auth", authRoute);
+
+app.listen(8800, () => {
+    console.log("Backend Server is Running")
+});
